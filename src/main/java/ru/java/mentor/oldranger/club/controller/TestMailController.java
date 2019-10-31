2932976c8887462b5f19e832d1224e8d74289a79
SvishD <d.svishchuk@gmail.com>
2019-10-31 19:21:36 +0300

    rename TestMailController

--- src/main/java/ru/java/mentor/oldranger/club/controller/TestMailController.java
+++ src/main/java/ru/java/mentor/oldranger/club/controller/TestMailController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import ru.java.mentor.oldranger.club.service.mail.MailService;
 
 @Controller
-public class MailSenderTestController {
+public class TestMailController {
 
     @Autowired
     MailService mailService;
 
+    // Пример работы с отправлением почты
     @GetMapping("/testmail")
     public String sendTestMail() {
         mailService.send("daref67649@3dmail.top","test","Lorem ipsum dolor sit amet");
