3d5b89c9024756cbeb0d0d95b86949f689ce4639
SvishD <d.svishchuk@gmail.com>
2019-10-31 18:15:46 +0300

    fixed imports

--- src/main/java/ru/java/mentor/oldranger/club/controller/MailSenderTestController.java
+++ src/main/java/ru/java/mentor/oldranger/club/controller/MailSenderTestController.java
@@ -3,7 +3,7 @@ package ru.java.mentor.oldranger.club.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.java.mentor.oldranger.club.sevice.MailService.MailService;
+import ru.java.mentor.oldranger.club.service.mail.MailService;
 
 @Controller
 public class MailSenderTestController {
