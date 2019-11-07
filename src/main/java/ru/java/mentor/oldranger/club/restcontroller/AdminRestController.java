65897934cf608902c775d9ba7b493526d485c0d4
ok213 <213331@gmail.com>
2019-11-07 12:45:59 +0300

    adminka v.01

--- src/main/java/ru/java/mentor/oldranger/club/restcontroller/AdminRestController.java
+++ src/main/java/ru/java/mentor/oldranger/club/restcontroller/AdminRestController.java
@@ -2,13 +2,12 @@ package ru.java.mentor.oldranger.club.restcontroller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.java.mentor.oldranger.club.dto.SectionsAndTopicsDto;
 import ru.java.mentor.oldranger.club.service.forum.ForumTreeAdminService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -22,4 +21,11 @@ public class AdminRestController {
         List<SectionsAndTopicsDto> dtos = forumTreeAdminService.getAllSectionAndAllTopics();
         return ResponseEntity.ok(dtos);
     }
+
+    @PatchMapping("/swapsections")
+    public ResponseEntity updateUser(@RequestBody Map<String, Long> swapSections) {
+        forumTreeAdminService.swapSectons(swapSections);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/ru/java/mentor/oldranger/club/service/forum/impl/ForumTreeAdminServiceImpl.java
+++ src/main/java/ru/java/mentor/oldranger/club/service/forum/impl/ForumTreeAdminServiceImpl.java
@@ -2,49 +2,47 @@ package ru.java.mentor.oldranger.club.service.forum.impl;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.java.mentor.oldranger.club.dao.ForumRepository.SectionRepository;
 import ru.java.mentor.oldranger.club.dto.SectionsAndTopicsDto;
 import ru.java.mentor.oldranger.club.model.forum.Section;
-import ru.java.mentor.oldranger.club.model.forum.Topic;
 import ru.java.mentor.oldranger.club.service.forum.ForumTreeAdminService;
+import ru.java.mentor.oldranger.club.service.forum.SectionsAndTopicsService;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
 public class ForumTreeAdminServiceImpl implements ForumTreeAdminService {
 
+    private SectionsAndTopicsService sectionsAndTopicsService;
+
+    private SectionRepository sectionRepository;
+
     @Override
     public List<SectionsAndTopicsDto> getAllSectionAndAllTopics() {
-
-        // временно для тестирования вывода данных из БД на страницу
-        Section section1 = new Section("1 section", 1, false);
-        List<Topic> topics1 = new ArrayList<>();
-        topics1.add(new Topic("1.1 topic", null, null, null, section1, false));
-        topics1.add(new Topic("1.2 topic", null, null, null, section1, false));
-        topics1.add(new Topic("1.3 topic", null, null, null, section1, false));
-
-        Section section2 = new Section("2 section", 2, false);
-        List<Topic> topics2 = new ArrayList<>();
-        topics2.add(new Topic("2.1 topic", null, null, null, section2, false));
-        topics2.add( new Topic("2.2 topic", null, null, null, section2, false));
-        topics2.add( new Topic("2.3 topic", null, null, null, section2, false));
-
-        Section section3 = new Section("3 section", 3, false);
-        List<Topic> topics3 = new ArrayList<>();
-        topics3.add(new Topic("3.1 topic", null, null, null, section3, false));
-        topics3.add(new Topic("3.2 topic", null, null, null, section3, false));
-        topics3.add(new Topic("3.3 topic", null, null, null, section3, false));
-
-        SectionsAndTopicsDto dto1 = new SectionsAndTopicsDto(section1, topics1);
-        SectionsAndTopicsDto dto2 = new SectionsAndTopicsDto(section2, topics2);
-        SectionsAndTopicsDto dto3 = new SectionsAndTopicsDto(section3, topics3);
-
-        List<SectionsAndTopicsDto> list = new ArrayList<>();
-        list.add(dto1);
-        list.add(dto2);
-        list.add(dto3);
-        return list;
+        return sectionsAndTopicsService.getAllSectionsAndActualTopicsLimit10BySection();
     }
+
+    @Override
+    public void swapSectons(Map<String, Long> swapSections) {
+        try {
+            Long id = swapSections.get("id1");
+            Long newPosition = swapSections.get("newPosition1");
+            Section section = sectionRepository.getOne(id);
+            section.setPosition(newPosition.intValue());
+            sectionRepository.save(section);
+
+            id = swapSections.get("id2");
+            newPosition = swapSections.get("newPosition2");
+            section = sectionRepository.getOne(id);
+            section.setPosition(newPosition.intValue());
+            sectionRepository.save(section);
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
