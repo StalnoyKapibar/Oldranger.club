7e4abfd9629261342d012e09e28c98f86e0a72a8
ok213 <213331@gmail.com>
2019-11-07 12:45:59 +0300

    adminka v.01

--- src/main/java/ru/java/mentor/oldranger/club/service/forum/ForumTreeAdminService.java
+++ src/main/java/ru/java/mentor/oldranger/club/service/forum/ForumTreeAdminService.java
@@ -3,7 +3,9 @@ package ru.java.mentor.oldranger.club.service.forum;
 import ru.java.mentor.oldranger.club.dto.SectionsAndTopicsDto;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ForumTreeAdminService {
     List<SectionsAndTopicsDto> getAllSectionAndAllTopics();
+    void swapSectons(Map<String, Long> swapSections);
 }
--- src/main/resources/static/myscript.js
+++ src/main/resources/static/myscript.js
@@ -14,7 +14,6 @@ function getAllSectionsAndTopics() {
             }
 
             response.json().then(function (data) {
-                // console.log(data);
                 window.allSectionsAndTopics = data;
                 showAllSectionsAndTopics();
             });
@@ -27,17 +26,18 @@ function getAllSectionsAndTopics() {
 
 function showAllSectionsAndTopics() {
     let li = '';
-    for (let i = 0; i < window.allSectionsAndTopics.length; i += 1) {
+    let sectionsLength = window.allSectionsAndTopics.length;
+    for (let i = 0; i < sectionsLength; i += 1) {
         let section = window.allSectionsAndTopics[i].section;
         let topics = window.allSectionsAndTopics[i].topics;
-        li += `<li id="${section.position}"><span>${section.name}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`;
-        if (i !== 0) {
-            li += `<button onclick="upSection(this)">+</button>&nbsp;`;
-        }
-        if (i !== window.allSectionsAndTopics.length - 1) {
-            li += `<button onclick="downSection(this)">-&nbsp;</button>`;
-        }
-        li += `<ul>`;
+        let btnPlusShow = (i == 0) ? 'style="visibility: hidden;"' : "";
+        let btnMinusShow = (i == sectionsLength - 1) ? 'style="visibility: hidden;"' : "";
+        li +=
+`<li id="${section.position}">
+    <button class="btnPlus" onclick="swapSections(this)" ${btnPlusShow}>+</button>&nbsp;
+    <button class="btnMinus" onclick="swapSections(this)" ${btnMinusShow}>-&nbsp;</button>&nbsp;&nbsp;&nbsp;
+    <span  style="font-weight: bold">${section.name}</span><ul>`;
+
         for (let j = 0; j < topics.length; j += 1) {
             let topic = topics[j];
             li += `<li id="${section.position}.${topic.id}">${topic.name}</li>`;
@@ -47,26 +47,67 @@ function showAllSectionsAndTopics() {
     document.getElementById('sections').innerHTML = li;
 }
 
-function downSection(btn) {
-    // здесь fetch на изменение данных в базе
-    // после изменение на экране
-
+function swapSections(btn) {
     let arr = window.allSectionsAndTopics;
-    let index = btn.parentElement.id - 1;
-    [arr[index + 1], arr[index]] = [arr[index], arr[index + 1]];
-    arr[index].section.position = index + 1;
-    arr[index + 1].section.position = index + 2;
-    showAllSectionsAndTopics();
-}
+    let li = btn.parentElement;
+    let btnClass = btn.className;
+    let swapSections = {};
+    let id1, newPosition, id2, oldPosition;
 
-function upSection(btn) {
-    // здесь fetch на изменение данных в базе
-    // после изменение на экране
+    if (btnClass === "btnMinus") {
+        id1 = arr[li.id - 1].section.id;
+        newPosition = +li.id;
+        id2 = arr[li.id].section.id;
+        oldPosition = newPosition - 1;
 
-    let arr = window.allSectionsAndTopics;
-    let index = btn.parentElement.id - 1;
-    [arr[index - 1], arr[index]] = [arr[index], arr[index - 1]];
-    arr[index].section.position = index + 1;
-    arr[index - 1].section.position = index;
-    showAllSectionsAndTopics();
+        swapSections = {
+            "id1": id1,
+            "newPosition1": newPosition + 1,
+            "id2": id2,
+            "newPosition2": newPosition
+        };
+    }
+
+    if (btnClass === "btnPlus") {
+        id1 = arr[li.id - 1].section.id;
+        newPosition = li.id - 2;
+        id2 = arr[li.id - 2].section.id;
+        oldPosition = newPosition + 1;
+
+        swapSections = {
+            "id1": id1,
+            "newPosition1": newPosition + 1,
+            "id2": id2,
+            "newPosition2": newPosition + 2
+        };
+    }
+
+    fetch("/api/admin/swapsections", {
+        method: "PATCH",
+        body: JSON.stringify(swapSections),
+        headers: {'Accept': 'application/json, text/plain',
+                  'Content-type': 'application/json;charset=UTF-8'}
+    })
+        .then(function(response){
+            if (response.status !== 200) {
+                alert('Error receiving data. Status Code: ' +  response.status);
+                return;
+            }
+
+            if (btnClass === "btnMinus") {
+                [arr[newPosition], arr[oldPosition]] = [arr[oldPosition], arr[newPosition]];
+                arr[newPosition].section.position = newPosition + 1;
+                arr[oldPosition].section.position = oldPosition + 1;
+            }
+            if (btnClass === "btnPlus") {
+                [arr[newPosition], arr[oldPosition]] = [arr[oldPosition], arr[newPosition]];
+                arr[newPosition].section.position = newPosition + 1;
+                arr[oldPosition].section.position = oldPosition + 1;
+            }
+            showAllSectionsAndTopics();
+        })
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+
 }
