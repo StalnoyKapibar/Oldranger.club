58e9bdefa94cf9ea26dc4e1030692e70f87af3c7
ok213 <213331@gmail.com>
2019-11-11 17:16:11 +0300

    добавил частично сортировку подсекций на тестовых данных

--- src/main/java/ru/java/mentor/oldranger/club/config/SecurityConfiguration.java
+++ src/main/java/ru/java/mentor/oldranger/club/config/SecurityConfiguration.java
@@ -18,7 +18,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -57,12 +56,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable() //наверное, временно?
-
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()             // для теста !!!
-                .antMatchers("/test/**","/img/**").permitAll()
+                .antMatchers("/test/**", "/img/**", "/css/**", "/js/**", "/image/**").permitAll()
                 .expressionHandler(webExpressionHandler())
+                .antMatchers("/", "/api/**").permitAll()
+                .antMatchers("/com/*", "/css/**", "/js/*", "/dist-smile/*").permitAll()
                 .antMatchers("/", "/api/**", "/passwordrecovery/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
