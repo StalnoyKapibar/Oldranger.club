630c4ca60141c95f472ed29cbfda9a9fe9e21f59
zveno65 <ziryanov2@mail.ru>
2019-11-23 20:26:16 +0500

    prop.sample

--- src/main/java/ru/java/mentor/oldranger/club/config/AppConfig.java
+++ src/main/java/ru/java/mentor/oldranger/club/config/AppConfig.java
@@ -7,9 +7,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableScheduling
 @PropertySource({
-        "classpath:config/datasource.properties",
-        "classpath:config/jpa.properties",
-        "classpath:config/mail.properties"/*,
+        "classpath:config/datasource.properties.sample",
+        "classpath:config/jpa.properties.sample",
+        "classpath:config/mail.properties.sample"/*,
         "classpath:config/cache.properties"*/
 })
 public class AppConfig {
