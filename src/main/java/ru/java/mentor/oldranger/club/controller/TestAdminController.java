4d0b85e263ebe26588d42cb6d368ccfa1d09b662
SvishD <d.svishchuk@gmail.com>
2019-12-02 15:18:51 +0300

    admin mail and drafts page

--- src/main/java/ru/java/mentor/oldranger/club/controller/TestAdminController.java
+++ src/main/java/ru/java/mentor/oldranger/club/controller/TestAdminController.java
@@ -9,9 +9,21 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ru.java.mentor.oldranger.club.model.user.User;
 import ru.java.mentor.oldranger.club.model.user.UserStatistic;
+import ru.java.mentor.oldranger.club.model.utils.EmailDraft;
 import ru.java.mentor.oldranger.club.service.chat.MessageService;
+import ru.java.mentor.oldranger.club.service.mail.EmailDraftService;
+import ru.java.mentor.oldranger.club.service.mail.MailService;
+import ru.java.mentor.oldranger.club.service.user.UserService;
 import ru.java.mentor.oldranger.club.service.user.UserStatisticService;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -20,6 +32,9 @@ public class TestAdminController {
 
     UserStatisticService userStatisticService;
     MessageService messageService;
+    EmailDraftService emailDraftService;
+    MailService mailService;
+    UserService userService;
 
     @GetMapping("/users")
     public String getAllUsers(Model model,
@@ -44,7 +59,7 @@ public class TestAdminController {
         model.addAttribute("pageCount", users.getTotalPages());
         model.addAttribute("usersList", users.getContent());
         model.addAttribute("query", query);
-        return "users";
+        return "admin/users";
     }
 
     // в админке получить список сеций и подсекций с возможностью сортировки
@@ -55,7 +70,7 @@ public class TestAdminController {
 
     @GetMapping("/chat")
     public String getChatSettings() {
-        return "chatSettings";
+        return "admin/chatSettings";
     }
 
     @PostMapping("/chat")
@@ -64,4 +79,62 @@ public class TestAdminController {
         return "redirect:/admin/chatSettings";
     }
 
+    @GetMapping("/mail")
+    public String getMailPage(Model model,
+                              @RequestParam(value = "editDraft",required = false) EmailDraft draft,
+                              @RequestAttribute(value = "page", required = false) Integer page,
+                              @PageableDefault(size = 5, sort = "lastEditDate", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        if (page != null) {
+            pageable = PageRequest.of(page, 5, Sort.by("lastEditDate").descending());
+        }
+        Page<EmailDraft> drafts = emailDraftService.getAllDraftsPageable(pageable);
+        if (draft != null){
+            model.addAttribute("draft", draft);
+        } else {
+            model.addAttribute("draft", new EmailDraft());
+        }
+        model.addAttribute("drafts", drafts);
+        model.addAttribute("draftList", drafts.getContent());
+        model.addAttribute("pageCount", drafts.getTotalPages());
+        return "admin/mail";
+    }
+
+    @PostMapping("/sendMail")
+    public String sendMail(EmailDraft draft) {
+        List<User> users = userService.findAll();
+        List<String> mailList = new ArrayList<>();
+        users.forEach(user -> mailList.add(user.getEmail()));
+        String[] emails = (String[]) mailList.toArray();
+        try {
+            mailService.sendHtmlMessage(emails, draft);
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/admin/mail";
+    }
+
+    @GetMapping("/editDraft/{id}")
+    public String editDraft(@PathVariable long id, RedirectAttributes ra) {
+        EmailDraft draft = emailDraftService.getById(id);
+        ra.addAttribute("editDraft",draft);
+        return "redirect:/admin/mail";
+    }
+
+    @GetMapping("/deleteDraft/{id}")
+    public String deleteDraft(@PathVariable long id) {
+        try {
+            emailDraftService.deleteDraft(id);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return "redirect:/admin/mail";
+    }
+
+    @PostMapping("/saveDraft")
+    public String saveDraft(EmailDraft draft) {
+        draft.setLastEditDate(LocalDateTime.now());
+        emailDraftService.saveDraft(draft);
+        return "redirect:/admin/mail";
+    }
 }
---
+++ src/main/java/ru/java/mentor/oldranger/club/model/utils/EmailDraft.java
@@ -0,0 +1,33 @@
+package ru.java.mentor.oldranger.club.model.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "email_drafts")
+public class EmailDraft {
+
+    @Id
+    @Column(name = "id_black_list")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "message", columnDefinition="TEXT")
+    private String message;
+
+    @Column(name = "last_edit")
+    private LocalDateTime lastEditDate;
+
+}
