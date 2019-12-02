d0ca128e2eb1f6f05f256a3a8a41f917a5bb9078
Vasiliy <Daemods@gmail.com>
2019-12-02 15:39:09 +0300

    fix

--- src/main/java/ru/java/mentor/oldranger/club/restcontroller/SectionsAndTopicsRestController.java
+++ src/main/java/ru/java/mentor/oldranger/club/restcontroller/SectionsAndTopicsRestController.java
@@ -1,7 +1,6 @@
 package ru.java.mentor.oldranger.club.restcontroller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
