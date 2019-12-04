6e37c0cdfb077bb7a6a3b8764c2be1269f5cce79
Vasiliy <Daemods@gmail.com>
2019-12-04 16:08:46 +0300

    Clean

--- src/main/java/ru/java/mentor/oldranger/club/dao/ChatRepository/ChatRepository.java
+++ src/main/java/ru/java/mentor/oldranger/club/dao/ChatRepository/ChatRepository.java
@@ -3,7 +3,6 @@ package ru.java.mentor.oldranger.club.dao.ChatRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.java.mentor.oldranger.club.model.chat.Chat;
-import ru.java.mentor.oldranger.club.model.user.User;
 
 import java.util.List;
 
