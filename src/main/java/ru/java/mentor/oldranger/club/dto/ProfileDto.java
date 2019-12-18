57825d9288160b293894f86de1abd83e2923e63d
gn0m3r <gn0m3r.ru@gmail.com>
2019-12-19 00:03:07 +0300

    Микрофикс ProfileDTO для фронта

--- src/main/java/ru/java/mentor/oldranger/club/dto/ProfileDto.java
+++ src/main/java/ru/java/mentor/oldranger/club/dto/ProfileDto.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class ProfileDto {
 
+    private long id;
     private String nickName;
     private String firstName;
     private String lastName;
