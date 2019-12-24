160a6018b7c9bdda962e73eb0ce488987302c7b7
gn0m3r <33595269+gn0m3r@users.noreply.github.com>
2019-12-24 05:55:27 +0300

    Update CommentAndTopicRestController.java

--- src/main/java/ru/java/mentor/oldranger/club/restcontroller/CommentAndTopicRestController.java
+++ src/main/java/ru/java/mentor/oldranger/club/restcontroller/CommentAndTopicRestController.java
@@ -100,7 +100,7 @@ public class CommentAndTopicRestController {
             summary = "Add a comment on topic", tags = { "Topic and comments" })
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
-                    content = @Content(schema = @Schema(implementation = Comment.class))),
+                    content = @Content(schema = @Schema(implementation = commentDto.class))),
             @ApiResponse(responseCode = "400",
                    description = "Error adding comment")})
     @PostMapping(value = "/comment/add", produces = { "application/json" })
@@ -149,7 +149,7 @@ public class CommentAndTopicRestController {
             summary = "Update a comment", tags = { "Topic and comments" })
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
-                    content = @Content(schema = @Schema(implementation = Comment.class))),
+                    content = @Content(schema = @Schema(implementation = CommentDto.class))),
             @ApiResponse(responseCode = "400", description = "Error updating comment")})
     @PutMapping(value = "/comment/update")
     public ResponseEntity<CommentDto> updateComment(@RequestBody JsonSavedMessageComentsEntity messageComments,
