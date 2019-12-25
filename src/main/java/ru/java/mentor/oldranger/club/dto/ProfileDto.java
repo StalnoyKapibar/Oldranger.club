cc9224f11b7312975b1d021e871b2139e1b8d84e
gn0m3r <gn0m3r.ru@gmail.com>
2019-12-25 16:42:53 +0300

    Added user_id to profileDTO

--- src/main/java/ru/java/mentor/oldranger/club/dto/ProfileDto.java
+++ src/main/java/ru/java/mentor/oldranger/club/dto/ProfileDto.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class ProfileDto {
 
+    private long user_id;
     private String nickName;
     private String firstName;
     private String lastName;
