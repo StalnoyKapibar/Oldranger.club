70cc0114e58310b862ad58f25633be7b97aa1a94
Vladimir Filippov <sergeiych@gmail.com>
2020-01-08 04:37:50 +0300

    'JsonSavedMessageComentsEntity' был переименован в 'CommentCreateUpdateDto' и перенесен в папку 'dto'

--- src/main/java/ru/java/mentor/oldranger/club/dto/CommentCreateAndUpdateDto.java
+++ src/main/java/ru/java/mentor/oldranger/club/dto/CommentCreateAndUpdateDto.java
@@ -12,7 +12,7 @@ public class CommentCreateAndUpdateDto {
 
     @Override
     public String toString() {
-        return "JsonSavedMessageComentsEntity{" +
+        return "CommentCreateAndUpdateDto{" +
                 "idTopic=" + idTopic +
                 ", text='" + text + '\'' +
                 ", answerID='" + answerID + '\'' +
