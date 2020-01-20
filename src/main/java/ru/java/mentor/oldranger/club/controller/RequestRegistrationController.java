4fcec37a8597d19f992efea926183475f252d998
Vladimir Filippov <sergeiych@gmail.com>
2020-01-20 22:06:15 +0300

    восстановлены ошибочно удаленные контроллеры

---
+++ src/main/java/ru/java/mentor/oldranger/club/controller/RequestRegistrationController.java
@@ -0,0 +1,21 @@
+package ru.java.mentor.oldranger.club.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Deprecated
+@Controller
+@Getter
+@Setter
+@AllArgsConstructor
+@RequestMapping("/request")
+public class RequestRegistrationController {
+    @GetMapping
+    public String getRequestForm() {
+        return "request";
+    }
+}
