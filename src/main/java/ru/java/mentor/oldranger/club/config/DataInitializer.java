e171243267297026672fd41f97364379bafecdf1
Яна ДемкивÐ <studioarttt@gmail.com>
2020-01-25 15:33:09 +0300

    Создан отдельный сервис и репозиторий для лайков. Исправлены ошибки. Удален метод getAll в ArticleRestController. Удален сет лайков в DataInitializer при создании статей

--- src/main/java/ru/java/mentor/oldranger/club/config/DataInitializer.java
+++ src/main/java/ru/java/mentor/oldranger/club/config/DataInitializer.java
@@ -253,7 +253,7 @@ public class DataInitializer implements CommandLineRunner {
             Set<ArticleTag> tags = new HashSet<>();
             tags.add(newsTags[i % 3]);
             articleService.addArticle(new Article("news", admin, tags, LocalDateTime.of(2019, 11, 1, 21, 33 + i, 35),
-                    "Text news!", false, new HashSet<>()));
+                    "Text news!", false));
         }
     }
 }
