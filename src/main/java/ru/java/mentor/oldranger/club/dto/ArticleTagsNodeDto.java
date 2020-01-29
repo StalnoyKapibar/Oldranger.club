898f0b4a6ad5b5d33bdf3971afd9633e4af6e8f7
Vladimir Filippov <sergeiych@gmail.com>
2020-01-29 23:13:23 +0300

    int[] вар очистили комменты

--- src/main/java/ru/java/mentor/oldranger/club/dto/ArticleTagsNodeDto.java
+++ src/main/java/ru/java/mentor/oldranger/club/dto/ArticleTagsNodeDto.java
@@ -24,46 +24,6 @@ import java.util.Arrays;
 public class ArticleTagsNodeDto {
     private Long id;
     private Long parentId;
-    private String  tag;
-    private String  tagsHierarchy;
-
-//    public ArticleTagsNodeDto(Long id, Long parentId, String tag, String tagsHierarchy) {
-//        this.id = id;
-//        this.parentId = parentId;
-//        this.tag = tag;
-//        this.tagsHierarchy = tagsHierarchy;
-//    }
-
-    //    private BigInteger id;
-//    private BigInteger parent;
-//    private BigInteger position;
-//    private BigInteger tag;
-
-
-//    private String tagsHierarchy;
-
-//    public ArticleTagsNodeDto(BigInteger id, ArticleTagsNode parent, Integer position, ArticleTag tag, String tagsHierarchy) {
-//    }
-
-
-//    public ArticleTagsNodeDto(ArticleTagsNode parent, int position, ArticleTag tag, tagsHierarchy) {
-//        this.parent = parent;
-//        this.position = position;
-//        this.tag = tag;
-//    }
-
-
-//    private Long id;
-//    private Long parentId;
-//    private ArticleTagsNode parent;
-//    private String  tag;
-//    private String  tagsHierarchy;
-//
-//    public ArticleTagsNodeDto(Long id, Long parentId, ArticleTagsNode  parent, String tag, String tagsHierarchy) {
-//        this.id = id;
-//        this.parentId = parentId;
-//        this.parent = parent;
-//        this.tag = tag == null ? "NULL" : tag;
-//        this.tagsHierarchy = tagsHierarchy;
-//    }
+    private String tag;
+    private int[] tagsHierarchy;
 }
--- src/main/java/ru/java/mentor/oldranger/club/service/article/impl/ArticleTagsNodeServiceImp.java
+++ src/main/java/ru/java/mentor/oldranger/club/service/article/impl/ArticleTagsNodeServiceImp.java
@@ -5,15 +5,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.java.mentor.oldranger.club.dao.ArticleRepository.ArticleTagsNodeRepository;
 import ru.java.mentor.oldranger.club.dto.ArticleTagsNodeDto;
-import ru.java.mentor.oldranger.club.model.article.ArticleTag;
 import ru.java.mentor.oldranger.club.model.article.ArticleTagsNode;
 import ru.java.mentor.oldranger.club.service.article.ArticleTagsNodeService;
 
-import javax.persistence.Tuple;
-import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,47 +25,13 @@ public class ArticleTagsNodeServiceImp implements ArticleTagsNodeService {
 
     @Override
     public List<ArticleTagsNodeDto> findHierarchyTreeOfAllTagsNodes() {
-        List<ArticleTagsNodeDto> dto  = new ArrayList<>();
-        for (Tuple i : tagsNodeRepository.findAllChildrenTree()) {
-            dto.add(new ArticleTagsNodeDto(
-                    i.get("id", Long.class),
-                    i.get("parent", Long.class),
-                    i.get("tag_name", String.class),
-                    i.get("tags_hierarchy", String.class)));
-            System.out.println(i);
-        }
-        return dto;
-
-//        return tagsNodeRepository.findAllChildrenTree().stream()
-//                .map(e -> new ArticleTagsNodeDto(e.getElements() tu ali.getId(),
-//                        e.getParent() == null ? -1L : e.getParent(),
-//                        e.getTag(),
-//                        Arrays.stream(e.getPosition().toString().split(",")).mapToInt(Integer::parseInt).toArray().toString()))
-//                .collect(Collectors.toList());
-
-//        return tagsNodeRepository.findAllChildrenTree();
-
-//        return tagsNodeRepository.findAllChildrenTree().stream()
-//                .map(e -> new ArticleTagsNodeDto(
-//                        (BigInteger) e .get(0),
-//                        (BigInteger)e.get(1),
-//                        (BigInteger)e.get(2),
-//                        (BigInteger) e.get(3))).collect(Collectors.toList());
-
-//        return tagsNodeRepository.findAllChildrenTree().stream()
-//                .map(e -> new ArticleTagsNodeDto(
-//                        (BigInteger) e .get(0),
-//                        (ArticleTagsNode)e.get(1),
-//                        (Integer)e.get(2),
-//                        (BigInteger) e.get(3),
-//                        (String) e.get(4))).collect(Collectors.toList());
-
-        //        return tagsNodeRepository.findAllChildrenTree();
-
-//        return tagsNodeRepository.findAllChildrenTree();
-
-
-     }
+                return tagsNodeRepository.findAllChildrenTree().stream()
+                .map(e -> new ArticleTagsNodeDto(
+                        Long.valueOf(e.get("id").toString()),
+                        e.get("parent") == null ? null :  Long.valueOf(e.get("parent").toString()),
+                        e.get("tag_name", String.class),
+                        Arrays.stream( e.get("tags_hierarchy", String.class).split(",")).mapToInt(Integer::parseInt).toArray())).collect(Collectors.toList());
+    }
 
     @Override
     public ArticleTagsNode findById(Long id) {
