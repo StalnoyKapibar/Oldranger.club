f76b848092ad58dd1695c4e94b066b520cf1f697
Яна ДемкивÐ <studioarttt@gmail.com>
2020-01-30 21:54:46 +0300

    Ñ Ð

--- src/main/java/ru/java/mentor/oldranger/club/dto/UsersTreeDto.java
+++ src/main/java/ru/java/mentor/oldranger/club/dto/UsersTreeDto.java
@@ -1,10 +1,24 @@
 package ru.java.mentor.oldranger.club.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.java.mentor.oldranger.club.model.user.User;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsersTreeDto {
 
-    private List<User> user;
+    private String nickName;
+    private Long inviteKey;
+    private Long userParentId;
+    private Long userId;
+
+    public UsersTreeDto(String nickName, Long userId) {
+        this.nickName = nickName;
+        this.userId = userId;
+    }
 }
