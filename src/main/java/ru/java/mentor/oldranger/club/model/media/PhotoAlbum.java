d5f4179c27343d72e0654cada13320d8671c3f64
Vladimir Filippov <sergeiych@gmail.com>
2020-02-02 06:26:24 +0300

    Исправлены имена таблицы

--- src/main/java/ru/java/mentor/oldranger/club/model/media/PhotoAlbum.java
+++ src/main/java/ru/java/mentor/oldranger/club/model/media/PhotoAlbum.java
@@ -25,13 +25,13 @@ public class PhotoAlbum {
     private String title;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "album_writers",
+    @JoinTable(name = "photo_album_writers",
             joinColumns = {@JoinColumn(name = "album_id")},
             inverseJoinColumns = {@JoinColumn(name = "user_id")})
     private Set<User> writers;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "album_viewers",
+    @JoinTable(name = "photo_album_viewers",
             joinColumns = {@JoinColumn(name = "album_id")},
             inverseJoinColumns = {@JoinColumn(name = "user_id")})
     private Set<User> viewers;
