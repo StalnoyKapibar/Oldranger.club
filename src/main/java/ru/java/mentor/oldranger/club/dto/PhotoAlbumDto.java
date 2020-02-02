52ba8d76f085f7e68ca29e4ecb6792ea5cbf2621
Vladimir Filippov <sergeiych@gmail.com>
2020-02-02 19:16:23 +0300

    Добавлен ДТО

---
+++ src/main/java/ru/java/mentor/oldranger/club/dto/PhotoAlbumDto.java
@@ -0,0 +1,14 @@
+package ru.java.mentor.oldranger.club.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+//ToDo дописать description и requiredProperties
+@Schema(description = "...ToDo... ",
+        requiredProperties = {".ToDo..", ".ToDo.."})
+public class PhotoAlbumDto {
+
+}
