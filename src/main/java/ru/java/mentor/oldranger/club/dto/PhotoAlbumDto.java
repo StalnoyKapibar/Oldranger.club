9dab0ebb64fea8c27e3b6a41206202c543306424
Vladimir Filippov <sergeiych@gmail.com>
2020-02-03 03:59:29 +0300

    Добавлен ДТО инициализация

--- src/main/java/ru/java/mentor/oldranger/club/dto/PhotoAlbumDto.java
+++ src/main/java/ru/java/mentor/oldranger/club/dto/PhotoAlbumDto.java
@@ -3,6 +3,8 @@ package ru.java.mentor.oldranger.club.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import ru.java.mentor.oldranger.club.model.media.Photo;
+import ru.java.mentor.oldranger.club.model.media.PhotoAlbum;
 
 @Data
 @AllArgsConstructor
@@ -10,5 +12,6 @@ import lombok.Data;
 @Schema(description = "...ToDo... ",
         requiredProperties = {".ToDo..", ".ToDo.."})
 public class PhotoAlbumDto {
-
+    private PhotoAlbum album;
+    private Photo mainPhoto;
 }
