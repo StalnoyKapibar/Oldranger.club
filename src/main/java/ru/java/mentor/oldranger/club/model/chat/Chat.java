7f4a7f1152a989426633aad07a0c7464e6454c59
Vladimir Filippov <sergeiych@gmail.com>
2020-02-07 17:09:50 +0300

    Поставлены комментарии по проблемным местам по "N+1 select" и "Cartasian Prodact"  проблемам

--- src/main/java/ru/java/mentor/oldranger/club/model/chat/Chat.java
+++ src/main/java/ru/java/mentor/oldranger/club/model/chat/Chat.java
@@ -1,8 +1,6 @@
 package ru.java.mentor.oldranger.club.model.chat;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 import ru.java.mentor.oldranger.club.model.media.PhotoAlbum;
 import ru.java.mentor.oldranger.club.model.user.User;
 
