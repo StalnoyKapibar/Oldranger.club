99f14bead07de1de1ba7d4955f3401b8a461da1e
Vladimir Filippov <sergeiych@gmail.com>
2020-02-14 16:08:45 +0300

    исправил запросы

---
+++ src/test/java/ru/java/mentor/oldranger/club/service/user/impl/PasswordRecoveryServiceImplTest.java
@@ -0,0 +1,12 @@
+package ru.java.mentor.oldranger.club.service.user.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordRecoveryServiceImplTest {
+
+    @Test
+    void sendRecoveryTokenToEmail() {
+    }
+}
