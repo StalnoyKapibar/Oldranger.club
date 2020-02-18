d422afc4c6544e1a65d023b5049e8ea2e3eede14
Яна ДемкивÐ <studioarttt@gmail.com>
2020-02-18 20:46:08 +0300

    Filter html comment.

---
+++ src/main/java/ru/java/mentor/oldranger/club/service/utils/impl/FilterHtmlServiceImpl.java
@@ -0,0 +1,15 @@
+package ru.java.mentor.oldranger.club.service.utils.impl;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import ru.java.mentor.oldranger.club.service.utils.FilterHtmlService;
+
+public class FilterHtmlServiceImpl implements FilterHtmlService {
+    @Override
+    public String filterHtml(String comment) {
+        Whitelist whitelist = new Whitelist();
+        whitelist.addTags("h1", "h2", "h3", "h4", "h5", "ol", "li", "u", "s", "blockquote", "em", "strong", "i", "b", "p", "a", "br");
+        whitelist.addAttributes(":all", "style");
+        return Jsoup.clean(comment, whitelist);
+    }
+}
