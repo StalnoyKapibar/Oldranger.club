8fcc4d0f16b16d7f9ca04858d104229ecfee4df6
Яна ДемкивÐ <studioarttt@gmail.com>
2020-02-24 19:50:31 +0300

    Изменен метод getAllByArticle в сервисе. ArtcileCommentRepository возвращает ArticleCommentDto.

--- src/main/java/ru/java/mentor/oldranger/club/dao/ArticleRepository/ArticleCommentRepository.java
+++ src/main/java/ru/java/mentor/oldranger/club/dao/ArticleRepository/ArticleCommentRepository.java
@@ -3,10 +3,14 @@ package ru.java.mentor.oldranger.club.dao.ArticleRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import ru.java.mentor.oldranger.club.dto.ArticleCommentDto;
 import ru.java.mentor.oldranger.club.model.article.Article;
 import ru.java.mentor.oldranger.club.model.comment.ArticleComment;
 
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
-    Page<ArticleComment> findByArticle(Article article, Pageable pageable);
+    @Query(value = "select new ru.java.mentor.oldranger.club.dto.ArticleCommentDto (ac.position, ac.id, ac.user, ac.dateTime, a.commentCount, rc.dateTime, rcu.nickName, ac.commentText, rc.commentText) \n" +
+            " from ArticleComment ac join ac.article a join ac.answerTo rc join rc.user rcu where a=:article")
+    Page<ArticleCommentDto> findByArticle(Article article, Pageable pageable);
 }
