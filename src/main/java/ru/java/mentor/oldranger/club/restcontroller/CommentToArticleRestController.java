0bf8d8075e07b48a070a721deb8dbc232a91e192
Яна ДемкивÐ <studioarttt@gmail.com>
2020-02-24 19:50:31 +0300

    Изменен метод getAllByArticle в сервисе. ArtcileCommentRepository возвращает ArticleCommentDto.

--- src/main/java/ru/java/mentor/oldranger/club/restcontroller/CommentToArticleRestController.java
+++ src/main/java/ru/java/mentor/oldranger/club/restcontroller/CommentToArticleRestController.java
@@ -59,7 +59,9 @@ public class CommentToArticleRestController {
         }
 
         Pageable pageable = PageRequest.of(page, 10, Sort.by("id"));
+        //
         Page<ArticleCommentDto> articleComments = articleService.getAllByArticle(article, pageable);
+        //
         ArticleAndCommentsDto articleAndCommentsDto = new ArticleAndCommentsDto(article, articleComments);
         return ResponseEntity.ok(articleAndCommentsDto);
     }
