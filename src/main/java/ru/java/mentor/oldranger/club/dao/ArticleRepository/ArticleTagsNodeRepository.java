24a7c66dfbd234f67d89f4497c4f47c801b2bbbf
Vladimir Filippov <sergeiych@gmail.com>
2020-02-25 18:25:11 +0300

    hot fix - добалено забытое поле

--- src/main/java/ru/java/mentor/oldranger/club/dao/ArticleRepository/ArticleTagsNodeRepository.java
+++ src/main/java/ru/java/mentor/oldranger/club/dao/ArticleRepository/ArticleTagsNodeRepository.java
@@ -63,6 +63,7 @@ public interface ArticleTagsNodeRepository extends JpaRepository<ArticleTagsNode
                 .map(e -> new ArticleTagsNodeDto(
                         Long.valueOf(e.get("id").toString()),
                         e.get("parent") == null ? null :  Long.valueOf(e.get("parent").toString()),
+                        Integer.valueOf(e.get("id").toString()),
                         e.get("tag_name", String.class),
                         Arrays.stream( e.get("tags_hierarchy", String.class).split(",")).mapToInt(Integer::parseInt).toArray())).collect(Collectors.toList());
     }
