bedb2320a525fc9bbc5b6ea3e78f1631d5b1ad9f
Яна ДемкивÐ <studioarttt@gmail.com>
2020-02-26 20:30:06 +0300

    Изменения в репозитории тип entity.

--- src/main/java/ru/java/mentor/oldranger/club/dao/ArticleRepository/ArticleCommentRepository.java
+++ src/main/java/ru/java/mentor/oldranger/club/dao/ArticleRepository/ArticleCommentRepository.java
@@ -6,9 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.java.mentor.oldranger.club.dto.ArticleCommentDto;
 import ru.java.mentor.oldranger.club.model.article.Article;
-import ru.java.mentor.oldranger.club.model.comment.ArticleComment;
 
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends JpaRepository<ArticleCommentDto, Long> {
 
     @Query(value = "select new ru.java.mentor.oldranger.club.dto.ArticleCommentDto (ac.position, ac.id, ac.user, ac.dateTime, a.commentCount, rc.dateTime, rcu.nickName, ac.commentText, rc.commentText) \n" +
             " from ArticleComment ac join ac.article a join ac.answerTo rc join rc.user rcu where a=:article")
