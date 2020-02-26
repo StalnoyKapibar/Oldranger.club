bdbd4c815b2462ba02ea5e1ae51187b8c04f076a
Яна ДемкивÐ <studioarttt@gmail.com>
2020-02-26 21:01:47 +0300

    Изменения в репозитории тип entity.

--- src/main/java/ru/java/mentor/oldranger/club/dao/MediaRepository/PhotoCommentRepository.java
+++ src/main/java/ru/java/mentor/oldranger/club/dao/MediaRepository/PhotoCommentRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.java.mentor.oldranger.club.dto.PhotoCommentDto;
+import ru.java.mentor.oldranger.club.model.comment.PhotoComment;
 import ru.java.mentor.oldranger.club.model.media.Photo;
 
-public interface PhotoCommentRepository extends JpaRepository<PhotoCommentDto, Long> {
+public interface PhotoCommentRepository extends JpaRepository<PhotoComment, Long> {
 
     @Query(value = "select new ru.java.mentor.oldranger.club.dto.PhotoCommentDto (p.position, p.id, p.user, p.dateTime, ph.commentCount, p.commentText)\n" +
             "from PhotoComment p join p.photo ph where p.photo = :photo")
