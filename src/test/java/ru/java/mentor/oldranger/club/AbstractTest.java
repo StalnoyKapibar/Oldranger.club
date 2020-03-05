b6e0e1dfa66d656c2783fa6a9cbd7bd73882ada8
Danila <danilka3769@gmail.com>
2020-03-05 19:28:13 +0300

    1. Добавил AbstractTest 2. Изменил PhotoAlbumRestControllerIT, т.к. поменялся сам PhotoAlbumRestController

---
+++ src/test/java/ru/java/mentor/oldranger/club/AbstractTest.java
@@ -0,0 +1,14 @@
+package ru.java.mentor.oldranger.club;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@TestPropertySource("/config/datasource-test.properties")
+@AutoConfigureMockMvc
+public abstract class AbstractTest {
+}
