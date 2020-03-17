29f3b54a311d994f9b74fef03aa0acb080390a07
alanorhon <alanorthon@gmail.com>
2020-03-17 21:45:01 +0700

    Revert "Delete ArticleCommentDto.java"
    This reverts commit 79285ffb

---
+++ src/main/java/ru/java/mentor/oldranger/club/dto/ArticleCommentDto.java
@@ -0,0 +1,25 @@
+package ru.java.mentor.oldranger.club.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.java.mentor.oldranger.club.model.user.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArticleCommentDto {
+
+    private Long positionInArticle;
+    private Long articleId;
+    private User author;
+    private LocalDateTime commentDateTime;
+    private LocalDateTime replyDateTime;
+    private String replyNick;
+    private String replyText;
+    private String commentText;
+
+}
