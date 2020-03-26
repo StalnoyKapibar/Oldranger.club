b8e1560d9ff117bca59785de68d036b74d93b549
alanorhon <alanorthon@gmail.com>
2020-03-26 16:32:04 +0700

    Поправил отображение секций и подсекций для Анонимных пользователей

--- src/main/java/ru/java/mentor/oldranger/club/dao/ForumRepository/SectionRepository.java
+++ src/main/java/ru/java/mentor/oldranger/club/dao/ForumRepository/SectionRepository.java
@@ -1,5 +1,6 @@
 package ru.java.mentor.oldranger.club.dao.ForumRepository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.java.mentor.oldranger.club.model.forum.Section;
 
@@ -7,5 +8,5 @@ import java.util.List;
 
 public interface SectionRepository extends JpaRepository<Section, Long> {
 
-    List<Section> getAllByIsHideToAnonIsFalse();
+    List<Section> getAllByIsHideToAnonIsFalse(Sort position);
 }
