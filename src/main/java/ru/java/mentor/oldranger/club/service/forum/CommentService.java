7450198956dd92737e43da072eb745620a22e135
PavelK <pavielkuz@gmail.com>
2020-05-12 08:33:16 +0300

    Рефактор метода updateComment в CommentsAndTopicsRestContoller

--- src/main/java/ru/java/mentor/oldranger/club/service/forum/CommentService.java
+++ src/main/java/ru/java/mentor/oldranger/club/service/forum/CommentService.java
@@ -2,10 +2,13 @@ package ru.java.mentor.oldranger.club.service.forum;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
+import ru.java.mentor.oldranger.club.dto.CommentCreateAndUpdateDto;
 import ru.java.mentor.oldranger.club.dto.CommentDto;
 import ru.java.mentor.oldranger.club.dto.CommentDtoAndCountMessages;
 import ru.java.mentor.oldranger.club.model.comment.Comment;
 import ru.java.mentor.oldranger.club.model.forum.Topic;
+import ru.java.mentor.oldranger.club.model.media.Photo;
 import ru.java.mentor.oldranger.club.model.user.User;
 
 import java.util.List;
@@ -38,4 +41,16 @@ public interface CommentService {
     boolean isEmptyComment(String comment);
 
     List<Comment> getChildComment(Comment comment);
+
+    boolean ifUserAllowedToEditComment(Comment comment, MultipartFile image1, MultipartFile image2,
+                                       CommentCreateAndUpdateDto messageComments, Topic topic,
+                                       User currentUser, User user);
+
+    Comment setInfoIntoComment(Comment comment, CommentCreateAndUpdateDto messageComments);
+
+    CommentDto deletePhotoFromDto(List<Long> idDeletePhotos, List<Photo> photos, CommentDto commentDto);
+
+    CommentDto updatePhotos(MultipartFile image1, MultipartFile image2,
+                            Comment comment, Topic topic,
+                            CommentDto commentDto, List<Photo> photos);
 }
