507d51de31ca723d40a7608a092962dc249a9eeb
vetst <62384532+vetst@users.noreply.github.com>
2020-10-12 15:35:17 +0300

    first commit (#394)
    Co-authored-by: vetst <vetstv@gmail.com>

--- src/main/java/ru/java/mentor/oldranger/club/exceptions/GlobalRestExceptionHandler.java
+++ src/main/java/ru/java/mentor/oldranger/club/exceptions/GlobalRestExceptionHandler.java
@@ -1,22 +1,58 @@
 package ru.java.mentor.oldranger.club.exceptions;
 
-import org.apache.tomcat.util.http.fileupload.FileUploadBase;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.java.mentor.oldranger.club.dto.ErrorDto;
+import ru.java.mentor.oldranger.club.exceptions.apierror.ApiError;
 
-import java.util.HashMap;
-import java.util.Map;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestControllerAdvice
 public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<ErrorDto> maxUploadSizeExceededException(MaxUploadSizeExceededException e) {
-        //return ResponseEntity.ok(new ErrorDto("File size must not exceed 20Mb"));
         return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(new ErrorDto("File size must not exceed 20Mb"));
     }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String error = "Malformed JSON request";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+    }
+
+//Triggered when an object fails @Valid validation.
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        ApiError apiError = new ApiError(BAD_REQUEST);
+        apiError.setMessage("Validation error");
+        apiError.addValidationErrors(ex.getBindingResult().getFieldErrors());
+        apiError.addValidationError(ex.getBindingResult().getGlobalErrors());
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
+                                                                      WebRequest request) {
+        ApiError apiError = new ApiError(BAD_REQUEST);
+        apiError.setMessage(String.format("The parameter '%s' of value '%s' could not be converted to type '%s'", ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName()));
+        apiError.setDebugMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
 }
